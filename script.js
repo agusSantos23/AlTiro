e2bd2b542fabc66e68e33257246a68c9de3a8c4d
Agustin S Prieto Atienza <131393036+agusSantos23@users.noreply.github.com>
2024-01-10 16:36:03 +0100

    1.3

--- script.js
+++ script.js
@@ -1,57 +1,109 @@
+
+//inicalizar el juego
 function start(){
     const plano = document.getElementById("plano")
     const puntuacion = document.getElementById("puntuacion")
+    const derrota = document.getElementById("derrota")
+    const btnStar = document.getElementById("Start")
+    const ronda = document.getElementById("ronda")
+    const animal = document.querySelectorAll(".animal")
+
+    //saber cuantos gallos hay en pantalla
+    let animalesVivos = 0
+    //saber cuantos se han mostrado
+    let animalesCreados = 0
+    //numero de la ronda
+    let rondaNumero = 0
+
+    
+    //Eliminar los gallos de la partida anterior
+    animal.forEach(animal =>{
+        animal.remove()
+    })
+    
+    //quitar el titulo de derrota
+    derrota.classList = "ocultarperder"
+    //resetear la puntuacion
+    puntuacion.textContent = "0"
+    
+    //desactiva el empezar la partida
+    btnStar.style.display = "none"
+    
     
 
-    let patosVivos = 0
+
+
     
     
     
-    
-    const intervalo = setInterval(() => {
+    const intervalo1 = setInterval(() => {
         
-        
-
-        if (patosVivos < 4 ) {
-
-            const nuevoPato = new Pato("Pato " + patosVivos);
-            const patoVivo = nuevoPato.vida()
-
-            patoVivo.addEventListener("click", () => {
-                nuevoPato.muerte(puntuacion)
-                patosVivos--
+        if (animalesVivos < 4 ) {
+                
+            const nuevaTortuga = new Tortuga("Tortuga " + animalesCreados);
+            const tortugaViva = nuevaTortuga.vida()
+                
+            tortugaViva.addEventListener("click", () => {
+                nuevaTortuga.muerte(puntuacion)
+                animalesVivos--
             });
-
-            plano.appendChild(patoVivo)
-            patosVivos++
-
+                
+            plano.appendChild(tortugaViva)
+            animalesVivos++
+            animalesCreados++
+                
         } else {
+                            
+            clearInterval(intervalo1)
+            derrota.classList.remove("ocultarperder")
+            btnStar.style.display = "inline"
             
-            clearInterval(intervalo)
         }
+    }, 800)
 
+
+    const intervalo2 = setInterval(() => {
         
-
+        if (animalesVivos < 4 ) {
+                
+            const nuevoGallo = new Gallo("Gallo " + animalesCreados);
+            const galloVivo = nuevoGallo.vida()
+                
+            galloVivo.addEventListener("click", () => {
+                nuevoGallo.muerte(puntuacion)
+                animalesVivos--
+            });
+                
+            plano.appendChild(galloVivo)
+            animalesVivos++
+            animalesCreados++
+                
+        } else {
+                            
+            clearInterval(intervalo2)
+            derrota.classList.remove("ocultarperder")
+            btnStar.style.display = "inline"
+            
+        }
     }, 500)
-    
-    
+
 }
 
 
 
 
 
-
-class Pato{
+//el animal que aparece con 2 funciones
+class Tortuga{
     constructor(id){
         this.id = id
     }
 
     vida() {
-        const imgEtiqueta = document.createElement("img");
-        imgEtiqueta.src = "img/pato.png"
-        imgEtiqueta.alt = this.id
-        imgEtiqueta.classList = "pato"
+        const imgEtiqueta = document.createElement("div");
+        
+        imgEtiqueta.id = this.id
+        imgEtiqueta.classList = "animal tortuga"
 
          
 
@@ -62,9 +114,9 @@ class Pato{
     }
 
     muerte(puntuacion) {
-        const patoElemento = document.querySelector(`[alt="${this.id}"]`);
-        if (patoElemento) {
-            patoElemento.parentNode.removeChild(patoElemento)
+        const tortugaElemento = document.querySelector(`[id="${this.id}"]`);
+        if (tortugaElemento) {
+            tortugaElemento.parentNode.removeChild(tortugaElemento)
 
             let numero = parseInt(puntuacion.textContent)
             numero++
@@ -72,4 +124,37 @@ class Pato{
         }
     }
 }
+
+class Gallo{
+    constructor(id){
+        this.id = id
+    }
+
+    vida() {
+        const imgEtiqueta = document.createElement("div");
+        
+        imgEtiqueta.id = this.id
+        imgEtiqueta.classList = "animal gallo"
+
+         
+
+        imgEtiqueta.style.left = Math.floor(Math.random()*600).toString() + "px"
+        imgEtiqueta.style.bottom = Math.floor(Math.random()*600).toString() + "px"
+
+        return imgEtiqueta
+    }
+
+    muerte(puntuacion) {
+        const galloElemento = document.querySelector(`[id="${this.id}"]`);
+        if (galloElemento) {
+            galloElemento.parentNode.removeChild(galloElemento)
+
+            let numero = parseInt(puntuacion.textContent)
+            numero++
+            puntuacion.textContent = numero
+        }
+    }
+}
+
+
 
